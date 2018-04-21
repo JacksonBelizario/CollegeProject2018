199c103db3f134cfb73762da7621a698d6675f46
Jackson L. F. Belizário <jacksonluizzb@gmail.com>
2018-04-21 19:31:00 -0400

    Adicionado Bootstrap

--- index.php
+++ index.php
@@ -30,10 +30,10 @@ ini_set("include_path", $path);
 //Listar dados da empresa
 try {
 	//Instancia objeto PDO para conexão com banco de dados
-	$db = new PDO('pgsql:dbname=caixa;
-	host=aula-alcione.serv.comp.uems.br;
-	user=pw2018;
-	password=PW2018&');
+	$db = new PDO('pgsql:dbname=zjmyszer;
+	host=elmer.db.elephantsql.com;
+	user=zjmyszer;
+	password=rlYVx3LvH2ahGOXnBN6nEnXfrIAq3ful');
 } catch (PDOException $e) {
 	echo 'Connection failed: ' . $e->getMessage();
 }
@@ -50,6 +50,7 @@ try {
 	<meta name="description" content=""/>
 	<meta name="ROBOTS" content="NOINDEX, NOFOLLOW"/>
 	<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link href="adm/css.css" rel="stylesheet" type="text/css"/>
 	<script type="text/javascript" src="adm/javascript.js">
 	<!--
@@ -57,58 +58,67 @@ try {
 	</script>
 </head>
 <body>
-	<?php
-	if (isset($_SESSION['logado'])) {
-		if ($_SESSION['logado']) {
-			//Tratar a requisição POST
-			if (isset($_POST['id'])) {
-				switch ($_POST['id']) {
-					case 1:
-					{
-						//montagem do SQL de atualização
-						$sql = "update cadastro.cadastro_empresa set ";
-						$sql .= "razao_social='".$_POST['nome']."',";
-						$sql .= "denominacao_social='".$_POST['denominacao']."',";
-						$sql .= "endereco='".$_POST['end']."',";
-						$sql .= "cpf_cnpj='".$_POST['doc']."' where id=".$_POST['codigo'];
-						//executando SQL de update no banco de dados
-						$n = $db->exec($sql);
-						if ($n == 0) {
-							echo("Erro: ".$db->errorInfo());
+	<div class="container">
+		<?php
+		if (isset($_SESSION['logado'])) {
+			if ($_SESSION['logado']) {
+				//Tratar a requisição POST
+				if (isset($_POST['id'])) {
+					switch ($_POST['id']) {
+						case 1:
+						{
+							//montagem do SQL de atualização
+							$sql = "update cadastro.cadastro_empresa set ";
+							$sql .= "razao_social='".$_POST['nome']."',";
+							$sql .= "denominacao_social='".$_POST['denominacao']."',";
+							$sql .= "endereco='".$_POST['end']."',";
+							$sql .= "cpf_cnpj='".$_POST['doc']."' where id=".$_POST['codigo'];
+							//executando SQL de update no banco de dados
+							$n = $db->exec($sql);
+							if ($n == 0) {
+								echo("Erro: ".$db->errorInfo());
+							}
+							break;
 						}
-						break;
 					}
-				}
-			} ?>
-			Contador de Sessão: <?php echo($_SESSION['count']); ?><br/>
-			<a href="logout.php" >Terminar Sessão</a><br/>
-			<h1>Controle de Fluxo de caixa</h1>
-			<hr></hr>
-			<a href="<?php echo($n_arq); ?>?id=1" >Cadastro</a> | <a href="<?php echo($n_arq); ?>?id=2" >Lançamentos</a> | <a href="<?php echo($n_arq); ?>?id=3" >Relatórios</a>
-			<hr></hr>
-			<?php
-			//print_r ($_GET);
-			if (isset($_GET['id'])) {
-				switch ($_GET['id']) {
-					default:
-					{
-						echo("Bem Vindo ao Sistema de Controle de Fluxo de Caixa!");
-						break;
+				} ?>
+				<nav class="navbar navbar-light bg-light">
+					<a class="navbar-brand" href="#">Controle de Fluxo de Caixa</a>
+					<div class="form-inline my-2 my-lg-0">
+						<a href="logout.php" >Terminar Sessão</a><br/>
+					</form>
+				</nav>
+				<hr></hr>
+				<ul class="nav nav-pills">
+					<li class="nav-item"><a class="nav-link <?= ((isset($_GET['id']) && $_GET['id'] == 1) ? 'active' : '') ?>" href="<?= $n_arq ?>?id=1">Cadastro</a></li>
+					<li class="nav-item"><a class="nav-link <?= ((isset($_GET['id']) && $_GET['id'] == 2) ? 'active' : '') ?>" href="<?= $n_arq ?>?id=2">Lançamentos</a></li>
+					<li class="nav-item"><a class="nav-link <?= ((isset($_GET['id']) && $_GET['id'] == 3) ? 'active' : '') ?>" href="<?= $n_arq ?>?id=3">Relatórios</a></li>
+				</ul>
+				<hr></hr>
+				<?php
+				//print_r ($_GET);
+				if (isset($_GET['id'])) {
+					switch ($_GET['id']) {
+						default:
+						{
+							echo("Bem Vindo ao Sistema de Controle de Fluxo de Caixa!");
+							break;
+						}
+						case 1:
+						{
+							include("cadastro.php");
+							break;
+						}
 					}
-					case 1:
-					{
-						include("cadastro.php");
-						break;
-					}
-				}
-			} ?>
-			<?php
+				} ?>
+				<?php
+			} else {
+				include("login.php");
+			}
 		} else {
 			include("login.php");
 		}
-	} else {
-		include("login.php");
-	}
-	?>
+		?>
+	</div>
 </body>
 </html>
