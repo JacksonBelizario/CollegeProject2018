3331a0648684f51279e74c04bc5472549688983b
Jackson L. F. Belizário <jacksonluizzb@gmail.com>
2018-04-21 16:59:29 -0400

    Correção de Indentação

--- logout.php
+++ logout.php
@@ -1,8 +1,7 @@
 <?php
-	session_start();
-	//unset($_SESSION['count']);
-	//unset($_SESSION['logado']);
-	//unset($_SESSION['usuario']);
-	session_unset();
-	header('Location: index.php');
-?>
+    session_start();
+    //unset($_SESSION['count']);
+    //unset($_SESSION['logado']);
+    //unset($_SESSION['usuario']);
+    session_unset();
+    header('Location: index.php');
